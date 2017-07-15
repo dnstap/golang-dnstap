f7fba5c0bb339dba31820f8d0b932b1debb1d7e0
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2017-07-15 21:06:05 +0200

    Addresses and ports

--- CdnsFormat.go
+++ CdnsFormat.go
@@ -18,6 +18,7 @@ package dnstap
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"os"
 )
@@ -54,17 +55,30 @@ func cborString(s string) []byte {
 
 func cborInteger(i int) []byte {
 	var (
-		r bytes.Buffer
+		r   bytes.Buffer
+		tmp []byte
 	)
 	if i < 0 {
 		fmt.Fprintf(os.Stderr, "Negative integers not yet supported TODO\n")
 		os.Exit(1)
 	}
-	if i > 23 {
-		fmt.Fprintf(os.Stderr, "Big integers not yet supported TODO\n")
-		os.Exit(1)
+	if i <= 23 {
+		r.WriteByte(0 + byte(i)) // 0 = Major type 0
+	} else {
+		if i > 65535 {
+			fmt.Fprintf(os.Stderr, "Big integers not yet supported TODO\n")
+			os.Exit(1)
+		}
+		if i <= 255 {
+			r.WriteByte(0 + 24) // 0 = Major type 0	, 24 = additional byte
+			r.WriteByte(byte(i))
+		} else {
+			tmp = make([]byte, 2)
+			r.WriteByte(0 + 25)
+			binary.BigEndian.PutUint16(tmp, uint16(i))
+			r.Write(tmp)
+		}
 	}
-	r.WriteByte(0 + byte(i)) // 0 = Major type 0
 	return r.Bytes()
 }
 
@@ -111,11 +125,15 @@ func cborBreak() []byte {
 
 func CdnsFormat(dt *Dnstap) (out []byte, ok bool) {
 	var (
-		s     bytes.Buffer
-		dummy []byte
+		s               bytes.Buffer
+		clientAddr      []byte = nil
+		serverAddr      []byte = nil
+		dummy           []byte = nil
+		sourcePort      uint32 = 0
+		destinationPort uint32 = 0
 	)
 
-	dummy = make([]byte, 16)
+	// TODO : put dt.Type, dt.Identity and  dt.Version somewhere
 	if first {
 		s.Write(cborArray(3))
 		// File type ID
@@ -136,82 +154,129 @@ func CdnsFormat(dt *Dnstap) (out []byte, ok bool) {
 		first = false
 	}
 
-	// Write a block for each message
-	s.Write(cborMap(3))
-	// Block preamble
-	s.Write(cborInteger(0))
-	s.Write(cborMap(1))
-	s.Write(cborInteger(1))
-	s.Write(cborArray(2))
-	s.Write(cborInteger(0)) // TODO real seconds
-	s.Write(cborInteger(0)) // TODO real microseconds
-	// Block tables
-	s.Write(cborInteger(2))
-	s.Write(cborMap(4))
-	//    IP addresses
-	s.Write(cborInteger(0))
-	s.Write(cborArray(1))
-	s.Write(cborByteString(dummy)) // TODO put the real IP address
-	//   Class type
-	s.Write(cborInteger(1))
-	s.Write(cborArray(1))
-	s.Write(cborMap(2))
-	s.Write(cborInteger(0))
-	s.Write(cborInteger(1)) // Class IN
-	s.Write(cborInteger(1))
-	s.Write(cborInteger(15)) // Type MX
-	//   Name rdata
-	s.Write(cborInteger(2))
-	s.Write(cborArray(1))
-	b := make([]byte, 5)
-	b[0] = 3
-	b[1] = 'c'
-	b[2] = 'o'
-	b[3] = 'm'
-	b[4] = 0
-	s.Write(cborByteString(b)) // TODO real data
-	//   Query sig
-	s.Write(cborInteger(3))
-	s.Write(cborArray(1))
-	s.Write(cborMap(5))
-	//      Server address index
-	s.Write(cborInteger(0))
-	s.Write(cborInteger(1))
-	//      Server port
-	s.Write(cborInteger(1))
-	s.Write(cborInteger(11)) // TODO real port
-	//      Transport flags
-	s.Write(cborInteger(2))
-	s.Write(cborInteger(2)) // IPv6. TODO: use real ones
-	//      QR sig flags
-	s.Write(cborInteger(3))
-	s.Write(cborInteger(0))
-	//      QR DNS flags
-	s.Write(cborInteger(5))
-	s.Write(cborInteger(0))
-	// End of block tables
-	// Queries/Responses
-	s.Write(cborInteger(3))
-	s.Write(cborArray(1))
-	s.Write(cborMap(5))
-	//    Time
-	s.Write(cborInteger(0))
-	s.Write(cborInteger(0))
-	//    Client address index
-	s.Write(cborInteger(2))
-	s.Write(cborInteger(1))
-	//    Client port
-	s.Write(cborInteger(3))
-	s.Write(cborInteger(10)) // TODO put the real port
-	//    Transaction ID
-	s.Write(cborInteger(4))
-	s.Write(cborInteger(6)) // TODO put the real ID
-	//    Query signature index
-	s.Write(cborInteger(5))
-	s.Write(cborInteger(1))
-	// End of block
+	if *dt.Type == Dnstap_MESSAGE {
+		m := dt.Message
+		// Write a block for each message. TODO: buffer for a few blocks
+		s.Write(cborMap(3))
+		// Block preamble
+		s.Write(cborInteger(0))
+		s.Write(cborMap(1))
+		s.Write(cborInteger(1))
+		s.Write(cborArray(2))
+		s.Write(cborInteger(0)) // TODO real seconds
+		s.Write(cborInteger(0)) // TODO real microseconds
+		// Block tables
+		s.Write(cborInteger(2))
+		s.Write(cborMap(4))
+		//    IP addresses.
+		s.Write(cborInteger(0))
+		clientAddr = m.QueryAddress
+		serverAddr = m.ResponseAddress
+		if m.QueryPort != nil {
+			sourcePort = *m.QueryPort
+		}
+		if m.ResponsePort != nil {
+			destinationPort = *m.ResponsePort
+		}
+		if clientAddr == nil && serverAddr == nil {
+			fmt.Fprintf(os.Stderr, "No IP addresses at all in the message, I give in\n")
+			os.Exit(1)
+		}
+		s.Write(cborArray(2))
+		if clientAddr != nil && serverAddr != nil {
+			s.Write(cborByteString(clientAddr))
+			s.Write(cborByteString(serverAddr))
+		} else {
+			switch *m.SocketFamily {
+			case SocketFamily_INET:
+				dummy = make([]byte, 4)
+			case SocketFamily_INET6:
+				dummy = make([]byte, 16)
+			default:
+				fmt.Fprintf(os.Stderr, "Unknown socket family %d\n", *m.SocketFamily)
+				os.Exit(1)
+			}
+			if clientAddr == nil {
+				s.Write(cborByteString(dummy))
+				s.Write(cborByteString(serverAddr))
+			} else {
+				s.Write(cborByteString(clientAddr))
+				s.Write(cborByteString(dummy))
+			}
+		}
+		//   Class type
+		s.Write(cborInteger(1))
+		s.Write(cborArray(1))
+		s.Write(cborMap(2))
+		s.Write(cborInteger(0))
+		s.Write(cborInteger(15)) // Type MX
+		s.Write(cborInteger(1))
+		s.Write(cborInteger(1)) //  Class IN
+		//   Name rdata
+		s.Write(cborInteger(2))
+		s.Write(cborArray(1))
+		b := make([]byte, 5)
+		b[0] = 3
+		b[1] = 'c'
+		b[2] = 'o'
+		b[3] = 'm'
+		b[4] = 0
+		s.Write(cborByteString(b)) // TODO real data
+		//   Query sig
+		s.Write(cborInteger(3))
+		s.Write(cborArray(1))
+		s.Write(cborMap(5))
+		//      Server address index
+		s.Write(cborInteger(0))
+		s.Write(cborInteger(2))
+		//      Server port
+		s.Write(cborInteger(1))
+		s.Write(cborInteger(int(destinationPort)))
+		//      Transport flags
+		s.Write(cborInteger(2))
+		switch *m.SocketFamily {
+		case SocketFamily_INET:
+			s.Write(cborInteger(0))
+		case SocketFamily_INET6:
+			s.Write(cborInteger(2))
+		default:
+			fmt.Fprintf(os.Stderr, "Unknown socket family %d\n", *m.SocketFamily)
+			os.Exit(1)
+		}
+		//      QR sig flags
+		s.Write(cborInteger(3))
+		if m.QueryMessage != nil {
+			s.Write(cborInteger(1))
+		} else { // A response
+			s.Write(cborInteger(2))
+		}
+		//      QR DNS flags
+		s.Write(cborInteger(5))
+		s.Write(cborInteger(0))
+		// End of block tables
+		// Queries/Responses
+		s.Write(cborInteger(3))
+		s.Write(cborArray(1))
+		s.Write(cborMap(5))
+		//    Time
+		s.Write(cborInteger(0))
+		s.Write(cborInteger(0))
+		//    Client address index
+		s.Write(cborInteger(2))
+		s.Write(cborInteger(1))
+		//    Client port
+		s.Write(cborInteger(3))
+		s.Write(cborInteger(int(sourcePort)))
+		//    Transaction ID
+		s.Write(cborInteger(4))
+		s.Write(cborInteger(666)) // TODO put the real ID
+		//    Query signature index
+		s.Write(cborInteger(5))
+		s.Write(cborInteger(1))
+		// End of block
 
-	fmt.Fprintf(os.Stderr, "%d bytes emitted\n", len(s.Bytes()))
+		fmt.Fprintf(os.Stderr, "%d bytes emitted\n", len(s.Bytes()))
+	}
 	return s.Bytes(), true
 }
 
