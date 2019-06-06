48525dd7e5e065b2f9787973b4672d255dd1fffe
Chris Mikkelson <cmikk@fsi.io>
2019-06-06 16:17:41 -0500

    Update copyright notices

--- FrameStreamInput.go
+++ FrameStreamInput.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 by Farsight Security, Inc.
+ * Copyright (c) 2013-2019 by Farsight Security, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
